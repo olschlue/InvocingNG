4e698c48d8ef6ca21819f9ba348dbf7407e38ed5
olschlue <post@oschlueter.de>
2025-12-30 16:32:46 +0000

    Header box-shadow auskommentiert

--- public/includes/header.php
+++ public/includes/header.php
@@ -30,7 +30,7 @@
             color: white;
             padding: 20px 0;
             margin-bottom: 5px;
-            box-shadow: 0 2px 4px rgba(0,0,0,0.1);
+            /*box-shadow: 0 2px 4px rgba(0,0,0,0.1);*/
         }
         
         header h1 {
